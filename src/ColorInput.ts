678832cfd508f31268146cad4dcf6e8d98ce549e
Philip Johnson <p.zhao.johnson@gmail.com>
2025-03-04 21:31:12 -0500

    Add missing type attribute to color inputs

--- src/ColorInput.ts
+++ src/ColorInput.ts
@@ -4,6 +4,8 @@ export class ColorInput {
   readonly domNode = document.createElement('input');
 
   constructor() {
+    this.domNode.type = 'color';
+
     this.domNode.classList.add(styles['color-input']);
   }
 }
