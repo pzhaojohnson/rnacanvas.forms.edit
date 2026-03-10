f61007062c3037b2c8f24a4073fdab3d841b8911
Philip Johnson <p.zhao.johnson@gmail.com>
2026-03-10 18:39:04 -0400

    Give text inputs a black background and highlight yellow on focus

--- src/TextInputField.ts
+++ src/TextInputField.ts
@@ -7,6 +7,10 @@ export class TextInputField {
     this.#field = new Field(name, inputElement);
 
     this.domNode.style.cursor = 'text';
+
+    inputElement.addEventListener('focus', () => this.domNode.style.color = 'yellow');
+
+    inputElement.addEventListener('blur', () => this.domNode.style.color = '');
   }
 
   get domNode() {
